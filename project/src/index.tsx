4c44e9d4c6893c44f08e0240bafd581bf6f5dfde
Iris <i.casinteva@gmail.com>
2025-01-24 00:20:36 +0100

    gh-pages set up

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -1,25 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import { Provider } from 'react-redux';
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-import browserHistory from './browser-history';
-import App from './components/app/app';
-import HistoryRouter from './components/history-route';
-import { store } from './store';
-import { checkAuthorization } from './store/api-actions';
+import browserHistory from "./browser-history";
+import App from "./components/app/app";
+import HistoryRouter from "./components/history-route";
+import { store } from "./store";
+import { checkAuthorization } from "./store/api-actions";
 
 store.dispatch(checkAuthorization());
+
+const env = process.env.NODE_ENV;
+const basename = env === "development" ? "" : "/six-cities-html-academy";
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <HistoryRouter history={browserHistory}>
+      <HistoryRouter basename={basename} history={browserHistory}>
         <ToastContainer />
         <App />
       </HistoryRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById('root'));
+  document.getElementById("root")
+);
